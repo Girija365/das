33376beeadf6afc7022c8f903f00eafccf1e3696
Girija365 <girijanandandas98@gmail.com>
2024-11-04 21:59:49 +0530

    first commit

---
+++ Contact.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_yo5srgn';
+   const templateID = 'template_z0xfwqc';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
